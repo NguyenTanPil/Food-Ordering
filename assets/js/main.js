c22162901471b6038c4727bf7b1f841a55524ce8
NguyenTanPil <traila1008@gmail.com>
2021-01-30 16:37:07 +0700

    Change browse places by plider slider and add list restaurants

---
+++ assets/js/main.js
@@ -0,0 +1,40 @@
+// places starts
+new Splide( '.browse-places .splide', {
+	type: 'loop',
+	perPage: 6,
+	perMove: 1,
+	autoplay: 'true',
+	breakpoints: {
+		576: {
+			perPage: 1,
+		},
+		768: {
+			perPage: 2,
+		},
+		1200: {
+			perPage: 4,
+		},
+	}
+}).mount();
+// places ends
+// restaurants starts
+new Splide( '.restaurant .splide', {
+	type: 'loop',
+	perPage: 5,
+	perMove: 1,
+	breakpoints: {
+		576: {
+			perPage: 1,
+		},
+		992: {
+			perPage: 2,
+		},
+		1200: {
+			perPage: 3,
+		},
+		1400: {
+			perPage: 4,
+		}
+	}
+}).mount();
+// restaurants ends
